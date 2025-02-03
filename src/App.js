bbda38949f63460e09756750e91772403460e920
OyaYagiz <oya_yagiz@outlook.com>
2025-02-03 17:23:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import './Pokecard.css'
+import Pokedex from './Pokedex'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Pokemon Go Card Game</h1>
+      <Pokedex/>
+    </div>
+  )
+}
+
+export default App
